ab005540241b3dafc18969a2e2afe88bbfb5d777
FongMi <b8892778@gmail.com>
2025-11-27 20:49:30 +0800

    Add move aar tool

---
+++ move.bat
@@ -0,0 +1,14 @@
+@echo off
+set "source_dir=D:\Project\media\libraries"
+set "target_dir=D:\Project\TV\app\libs"
+set "move_list=D:\Project\media\move.txt"
+
+for /r "%source_dir%" %%a in (lib-*-release.aar) do (
+    findstr /x /c:"%%~nxa" "%move_list%" >nul
+    if not errorlevel 1 (
+        copy "%%a" "%target_dir%\"
+        echo Moved "%%a" to "%target_dir%"
+    ) else (
+        echo Skipped "%%a" as it is not listed in move.txt
+    )
+)
