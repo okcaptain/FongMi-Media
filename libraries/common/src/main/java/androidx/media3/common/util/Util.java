314466071cfeabfd1175d29b7b4c94b3c5c17250
FongMi <b8892778@gmail.com>
2025-11-28 11:11:18 +0800

    Optimize inferContentType

--- libraries/common/src/main/java/androidx/media3/common/util/Util.java
+++ libraries/common/src/main/java/androidx/media3/common/util/Util.java
@@ -2684,6 +2684,23 @@ public final class Util {
       return C.CONTENT_TYPE_RTSP;
     }
 
+    if ("data".equals(scheme)) {
+      if (uri.getSchemeSpecificPart().startsWith(MimeTypes.APPLICATION_MPD)) {
+        return C.CONTENT_TYPE_DASH;
+      } else {
+        return C.CONTENT_TYPE_HLS;
+      }
+    }
+
+    if ("proxy".equals(scheme)) {
+      String url = uri.toString();
+      if (url.contains("m3u8")) {
+        return C.CONTENT_TYPE_HLS;
+      } else if (url.contains("mpd")) {
+        return C.CONTENT_TYPE_DASH;
+      }
+    }
+
     @Nullable String lastPathSegment = uri.getLastPathSegment();
     if (lastPathSegment == null) {
       return C.CONTENT_TYPE_OTHER;
@@ -2714,6 +2731,14 @@ public final class Util {
       return C.CONTENT_TYPE_SS;
     }
 
+    if (uri.getPath() != null && uri.getPath().contains("m3u8") || uri.getQuery() != null && uri.getQuery().contains("m3u8")) {
+      return C.CONTENT_TYPE_HLS;
+    }
+
+    if (uri.getQueryParameter("type") != null && uri.getQueryParameter("type").contains("mpd")) {
+      return C.CONTENT_TYPE_DASH;
+    }
+
     return C.CONTENT_TYPE_OTHER;
   }
 
@@ -2738,6 +2763,7 @@ public final class Util {
     switch (fileExtension) {
       case "mpd":
         return C.CONTENT_TYPE_DASH;
+      case "php":
       case "m3u8":
         return C.CONTENT_TYPE_HLS;
       case "ism":
