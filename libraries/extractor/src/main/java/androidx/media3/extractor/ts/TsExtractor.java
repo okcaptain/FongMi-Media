3147e21c2b26426c24a54d8cfe72367a7cf41399
FongMi <b8892778@gmail.com>
2025-11-28 13:06:41 +0800

    Optimize find ts sync

--- libraries/extractor/src/main/java/androidx/media3/extractor/ts/TsExtractor.java
+++ libraries/extractor/src/main/java/androidx/media3/extractor/ts/TsExtractor.java
@@ -349,10 +349,26 @@ public final class TsExtractor implements Extractor {
     resetPayloadReaders();
   }
 
+  private void skip(ExtractorInput input) throws IOException {
+    int maxLength = TS_PACKET_SIZE * 5;
+    byte[] buffer = tsPacketBuffer.getData();
+    final int length = Math.min(buffer.length, maxLength + TS_PACKET_SIZE);
+    input.peekFully(buffer, 0, length);
+    for (int i = 0; i < length - TS_PACKET_SIZE; i++) {
+      if (buffer[i] == TS_SYNC_BYTE && buffer[i + TS_PACKET_SIZE] == TS_SYNC_BYTE) {
+        if (i > 0) {
+          input.skipFully(i);
+        }
+        return;
+      }
+    }
+  }
+
   // Extractor implementation.
 
   @Override
   public boolean sniff(ExtractorInput input) throws IOException {
+    skip(input);
     byte[] buffer = tsPacketBuffer.getData();
     input.peekFully(buffer, 0, TS_PACKET_SIZE * SNIFF_TS_PACKET_COUNT);
     for (int startPosCandidate = 0; startPosCandidate < TS_PACKET_SIZE; startPosCandidate++) {
@@ -583,7 +599,7 @@ public final class TsExtractor implements Extractor {
    * <p>This may be a position beyond the buffer limit if the packet has not been read fully into
    * the buffer, or if no packet could be found within the buffer.
    */
-  private int findEndOfFirstTsPacketInBuffer() throws ParserException {
+  private int findEndOfFirstTsPacketInBuffer() {
     int searchStart = tsPacketBuffer.getPosition();
     int limit = tsPacketBuffer.limit();
     int syncBytePosition =
@@ -594,10 +610,6 @@ public final class TsExtractor implements Extractor {
     int endOfPacket = syncBytePosition + TS_PACKET_SIZE;
     if (endOfPacket > limit) {
       bytesSinceLastSync += syncBytePosition - searchStart;
-      if (mode == MODE_HLS && bytesSinceLastSync > TS_PACKET_SIZE * 2) {
-        throw ParserException.createForMalformedContainer(
-            "Cannot find sync byte. Most likely not a Transport Stream.", /* cause= */ null);
-      }
     } else {
       // We have found a packet within the buffer.
       bytesSinceLastSync = 0;
